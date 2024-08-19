3eb572e61b4b92d83a52337b5caffbabfdb0cea0
mohammad sajjad <m.s.e.r1382@gmail.com>
2024-08-19 09:43:52 +0330

    feat(app):register middleware and handle REST errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,22 @@
 <?php
 
+use App\Exceptions\RestException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias(["token.role" => App\Http\Middleware\CheckTokenRole::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (RestException $exception) {
+            return response()->json(["message" => $exception->errorMessage, "timestamp" => $exception->timestamp], $exception->statusCode);
+        });
     })->create();
