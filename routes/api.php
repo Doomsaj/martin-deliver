4f21c276e726b18be2b264cf1a02d1de649a0bed
mohammad sajjad <m.s.e.r1382@gmail.com>
2024-08-19 09:47:51 +0330

    feat(route):added routing

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\ClientAuthController;
+use App\Http\Controllers\ClientRequestController;
+use App\Http\Controllers\CourierAuthController;
+
+Route::prefix("courier")->group(function () {
+    Route::post('login', [CourierAuthController::class, 'login']);
+    Route::middleware(['auth:sanctum', 'token.role:courier'])->group(function () {
+    });
+});
+
+Route::prefix("client")->group(function () {
+    Route::post('login', [ClientAuthController::class, 'login']);
+    Route::middleware(['auth:sanctum', 'token.role:client'])->group(function () {
+        Route::post("new-request", [ClientRequestController::class, 'placeNewConsignmentRequest']);
+        Route::post("cancel-request", [ClientRequestController::class, 'cancelRequest']);
+    });
+});
